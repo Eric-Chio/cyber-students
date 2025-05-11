c6bbc5af47031d345f12290aa854958c74a16744
Eric-Chio <ericchio666@gmail.com>
2025-05-11 21:06:43 +0100

    Add files via upload

--- api/conf.py
+++ api/conf.py
@@ -8,3 +8,10 @@ MONGODB_HOST = {
 MONGODB_DBNAME = 'cyberStudents'
 
 WORKERS = 32
+
+# AES-256 encryption key (Base64 encoded)
+import base64
+import os
+
+pepper = base64.b64decode("Q2Fycm90VGhpc0lzU2VjdXJlQmVjYXVzZUl0c0xvbmc=")
+secret_key = base64.b64decode("kUfLNs9a1h2dOVsbn0wEfkK1dsBzLKR4eTabpb0F7b4=")
